95305ee4ec841fd748a0556e0a7d7934f5fbb46d
Your Name <you@example.com>
2021-10-15 05:07:45 +0700

    Updates

--- js/chunk-2d22d746.126ef055.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,t){"use strict";t.r(e);var a=t("7a23"),c={class:"about"},s=Object(a["e"])("h1",null,"This is an about page",-1),u=[s];function d(n,e){return Object(a["q"])(),Object(a["d"])("div",c,u)}var o=t("6b0d"),b=t.n(o);const i={},r=b()(i,[["render",d]]);e["default"]=r}}]);
-//# sourceMappingURL=chunk-2d22d746.126ef055.js.map
